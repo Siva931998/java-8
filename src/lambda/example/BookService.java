62e0e1937424a12c87f5ddaf772e1b9ee2d2e985
sivasundaram <sivasundaram9398@gmail.com>
2024-08-13 15:25:59 +0530

    Functional interface with lambda expression

--- src/lambda/example/BookService.java
+++ src/lambda/example/BookService.java
@@ -9,7 +9,13 @@ public class BookService {
 
     public List<Book> getBookInserted(){
        List<Book> books =  new BookDAO().getBook();
-        Collections.sort(books, new MyBookComparator());
+       //lambda expression
+        Collections.sort(books, (o1,o2)-> o1.getName().compareTo(o2.getName()
+//            @Override
+//            public int compare(Book o1, Book o2) {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+        ));
         return books;
     }
 
@@ -18,11 +24,11 @@ public class BookService {
 
     }
 }
-class MyBookComparator implements Comparator<Book>{
+/*class MyBookComparator implements Comparator<Book>{
 
     @Override
     public int compare(Book o1, Book o2) {
         return o1.getName().compareTo(o2.getName());
     }
-}
+}*/
 
