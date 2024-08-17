3d7892383de4fa6a648c9c8ae6f86df1e4451376
sivasundaram <sivasundaram9398@gmail.com>
2024-08-17 14:45:30 +0530

    listSorting change the package name

--- src/StreamSorting/SortingList.java
+++ src/StreamSorting/SortingList.java
@@ -1,11 +1,10 @@
-package ListSorting;
+package StreamSorting;
 
 import Stream_Api.DAO;
 import Stream_Api.Employee;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SortingList {
 
