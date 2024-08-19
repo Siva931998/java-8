66e22d6dee7a6f94c24cba3ad6009fdc98e8be31
sivasundaram <sivasundaram9398@gmail.com>
2024-08-19 10:49:20 +0530

    change the code to get the user input

--- Interview_String_program/src/Calculationpower.java
+++ Interview_String_program/src/Calculationpower.java
@@ -1,9 +1,15 @@
+package src;
+
+import java.util.Scanner;
 
 public class Calculationpower {
 
 	public static void main(String[] args) {
 
-		int base = 3, exponent =4;
+//		int base = 3, exponent =4;
+		Scanner sc = new Scanner(System.in);
+		int base = sc.nextInt();
+		int exponent = sc.nextInt();
 		
 		long result = 1;
 		
